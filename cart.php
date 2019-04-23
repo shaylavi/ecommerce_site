db30ee40d065e4f354ae9775ef5dd0a5fd9472a4
shaylavi <shay@esriaustralia.com.au>
2019-04-23 23:42:31 +1000

    empty files for all pages, links on header sorted

---
+++ cart.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Buy green - save the planet! Be part of the change." />
+    <link rel="icon" href="favicon.ico" />
+
+    <title>
+    
+    </title>
+
+    <link rel="stylesheet" href="./css/style.css"/>
+    <link rel="stylesheet" href="./css/leaves.css"/>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+  </head>
+  <body>
+  
+  <?php include 'header.php'; ?>
+
+    <div>
+       
+        </div>
+
+        <?php include 'footer.php'; ?>
+
+      </body>
+</html>
--- header.php
+++ header.php
@@ -1,8 +1,8 @@
     <nav role="navigation" class="navbar navbar-light" style="background-color: white; margin-bottom: 0px; ">
       <div class="container-fluid">
         <div class="navbar-header">
-          <img src="https://s3.us-east-2.amazonaws.com/eco-travel/products/logo.png" />
-          <a class="navbar-brand" href="#">
+        <a href="index.php"><img src="https://s3.us-east-2.amazonaws.com/eco-travel/products/logo.png" /></a>
+          <a class="navbar-brand" href="index.php">
             Eco-Traveller
           </a>
           <div class="navbar-duplicate">
@@ -13,26 +13,26 @@
           <i></i>  <i></i>  <i></i>  <i></i>  <i></i>  <i></i>  <i></i>  <i></i>  <i></i>  <i></i>  <i></i>  <i></i>  <i></i>  <i></i>  <i></i>
         </div>        
         <ul class="nav navbar-nav navbar-right">
-          <li class="active"><a href="#">Home</a></li>
+          <li class="active"><a href="index.php">Home</a></li>
           <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="products.php">
               Categories<span class="caret"></span></a>
             <ul class="dropdown-menu">
-              <li><a href="#">Toiletries</a></li>
-              <li><a href="#">Clothes</a></li>
-              <li><a href="#">Kitchenware</a></li>
-              <li><a href="#">Bags</a></li>
-              <li><a href="#">Bamboo</a></li>
+              <li><a href="products.php?cat=Toiletries">Toiletries</a></li>
+              <li><a href="products.php?cat=Clothes">Clothes</a></li>
+              <li><a href="products.php?cat=Kitchenware">Kitchenware</a></li>
+              <li><a href="products.php?cat=Bags">Bags</a></li>
+              <li><a href="products.php?cat=Bamboo">Bamboo</a></li>
             </ul>
           </li>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Contact Us</a></li>
+          <li><a href="about.php">About Us</a></li>
+          <li><a href="contact.php">Contact Us</a></li>
           <li>
-            <a href="#"><span class="glyphicon glyphicon-shopping-cart"></span>
+            <a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>
             Cart</a>
           </li>
           <li>
-            <a href="#"><span class="glyphicon glyphicon-search"></span>
+            <a href="search.php"><span class="glyphicon glyphicon-search"></span>
             Search</a>
           </li>
           <!-- <form class="navbar-form navbar-right" action="/action_page.php">
