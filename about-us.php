0142b1813d2c0fbe8fcc5bc54746b9e3cf507b79
Nick <github@nick-hulley.com>
2019-04-25 22:37:21 +1000

    Added in about-us page, added in images folder

---
+++ about-us.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Buy green - save the planet! Be part of the change." />
+    <link rel="icon" href="favicon.ico" />
+
+    <title>About us</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="./css/style.css"/>
+    <link rel="stylesheet" href="./css/leaves.css"/>
+
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <link rel="stylesheet" href="./css/about-us-style.css"/>
+
+
+  </head>
+  <body>
+
+  <?php include 'header.php'; ?>
+
+  <main class="container about-us">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="text-center">About Us</h1>
+      </div>
+    </div>
+    <div class="row">
+      <img class="rounded" src="images/nature-image-01.jpg" alt="nature" class="hero-image">
+    </div>
+
+    <section class="container-fluid">
+      <h2 class="text-left">Who are we</h2>
+      <hr>
+      <div class="row d-flex">
+        <div class="col-md-8 order-1 order-sm-1">
+          <p>
+            We are a non-profit business who's primary objective is to make the world
+            a better place through selling only products that are environmentally friendly,
+            whether this is in the way they are made, or the source of its materials.
+          </p>
+          <p>
+            We do this to make the world a better place
+          </p>
+        </div>
+        <aside class="col-md-4 order-2 order-sm-2">
+          <img class="rounded" src="images/nature-image-02.jpg">
+        </div>
+      </div>
+    </section>
+
+    <section class="container-fluid">
+      <h2 class="text-left">What we do</h2>
+      <hr>
+      <div class="row reversed">
+        <div class="col-md-8">
+          <p>
+            We are a non-profit business who's primary objective is to make the world
+            a better place through selling only products that are environmentally friendly,
+            whether this is in the way they are made, or the source of its materials.
+          </p>
+          <p>
+            We do this to make the world a better place
+          </p>
+        </div>
+        <aside class="col-md-4">
+          <img class="rounded" src="images/plastic-trash-01.jpg">
+        </div>
+      </div>
+    </section>
+
+    <section class="container-fluid">
+      <h2 class="text-left">How can you help</h2>
+      <hr>
+      <div class="row d-flex">
+        <div class="col-md-8 order-1 order-sm-1">
+          <p>
+            We are a non-profit business who's primary objective is to make the world
+            a better place through selling only products that are environmentally friendly,
+            whether this is in the way they are made, or the source of its materials.
+          </p>
+          <p>
+            We do this to make the world a better place
+          </p>
+        </div>
+        <aside class="col-md-4 order-2 order-sm-2">
+          <img class="rounded" src="images/hiking-01.jpg">
+        </div>
+      </div>
+    </section>
+
+  </main>
+
+  <?php include 'footer.php'; ?>
+
+  </body>
+</html>
