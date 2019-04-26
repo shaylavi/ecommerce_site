5e31b419421f65370bf7d21eb4af78b8d8c684f6
shaylavi <shay@esriaustralia.com.au>
2019-04-26 10:37:46 +1000

    carousel demo text

--- carousel.php
+++ carousel.php
@@ -10,17 +10,36 @@
 
         <!-- Wrapper for slides -->
         <div class="carousel-inner carousel-style">
-          <div class="item active carousel-item">
-            <div class="carousel-item-1"></div>
+
+        <div class="item active carousel-item">
+            <div class="carousel-item-div carousel-item-1"></div>
             <div class="slider-text">
-              <div style="font-size:47px"><b>Be part of the change!</b></div>
-              <div class="col-md-8" style="font-size:40px">
-                In times where global warming is an issue, we need to come up with better description for this website. Come take part and save this website! Edit this text and make it better.
+              <div style="font-size:47px; color: black"><b>One step closer to nature</b></div>
+              <div class="col-md-8" style="font-size:40px; color: black">
+                By choosing eco-friendly products, you immediately help protecting our earth. Make a difference and choose plastic free products!
               </div>
             </div>
           </div>
 
-          <div class="item carousel-item carousel-item-2"></div>
+          <div class="item carousel-item">
+            <div class="carousel-item-div carousel-item-2"></div>
+            <div class="slider-text">
+              <div style="font-size:47px; color: white"><b>We have only one home</b></div>
+              <div class="col-md-10" style="font-size:40px; color: white">
+                The ecological damages caused by humans
+              </div>
+            </div>
+          </div>
+          <!-- <div class="item active carousel-item">
+            <div class="carousel-item-div carousel-item-2"></div>
+            <div class="slider-text">
+            <div style="font-size:47px"><b>Be part of the change!</b></div>
+              <div class="col-md-8" style="font-size:40px">
+              In times where global warming is an issue, we need to come up with better description for this website. Come take part and save this website! Edit this text and make it better.
+              </div>
+            </div>
+          </div>
+           -->
           <div class="item carousel-item carousel-item-3"></div>
           <div class="item carousel-item carousel-item-4"></div>
           <div class="item carousel-item carousel-item-5"></div>
