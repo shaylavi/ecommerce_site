da8f9cb64168502b0e8655dcaa2a88cc711c505f
Nick Hulley <nick.hulley98@gmail.com>
2019-05-10 12:53:57 +1000

    Categories now dynamically added

---
+++ snippets/handle-get-categories.php
@@ -0,0 +1,14 @@
+<?php 
+    include '../db-connection.php';
+    $sqlStatement = "SELECT * FROM Categorys";
+    $result = makeQuery($sqlStatement);
+    $numberOfResults = $result->num_rows;
+    if ($numberOfResults > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $categoryID =$row["CategoryID"];
+            $categoryName =$row["Title"];
+            echo '<li><a href="products.php?cat=' . $categoryID . '">' . $categoryName . '</a></li>';
+            
+        }
+    }
+?>
